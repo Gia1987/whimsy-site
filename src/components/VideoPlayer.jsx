ffdacdc3ec0f3286e1d99bafb29e9df70ffbab5b
Gia1987 <giacomo.n27@gmail.com>
2024-09-30 15:27:44 +0100

    add video component, about page

---
+++ src/components/VideoPlayer.jsx
@@ -0,0 +1,13 @@
+export const VideoPlayer = ({src, poster}) => {
+<script>
+  console.log("Hi! I’m the client. This is logged in browser dev console.");
+</script>
+  return (
+    <div data-vjs-player>
+      <video controls preload="none" width="100%" poster={`${poster}`}>
+        <source src={`${src}`} type="video/mp4"/>
+      </video>
+    </div>
+  );
+};
+export default VideoPlayer;
