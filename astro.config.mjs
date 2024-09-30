56aa12d931bbd2785c053cd6aefcb315698503c2
Gia1987 <giacomo.n27@gmail.com>
2024-09-30 15:35:21 +0100

    add deployement instruction

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,7 @@ import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://Gia1987.github.io',
+  base: 'my-repo',
   integrations: [react()]
 });
