5927705958f525615fe13856046bdbdec9119b7d
Gia1987 <giacomo.n27@gmail.com>
2024-10-04 14:50:21 +0100

    remove base_url

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,9 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import react from '@astrojs/react';
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
   site: import.meta.env.PUBLIC_MY_DOMAIN,
-  base: import.meta.env.PUBLIC_BASE,
-  integrations: [react()]
+  integrations: [react()],
 });
-
