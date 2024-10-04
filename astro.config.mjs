b48a473f5ffa56afe7f276cf45e30fa97f8ccb6e
Gia1987 <giacomo.n27@gmail.com>
2024-10-04 16:10:43 +0100

    fix cname

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,10 +6,4 @@ import react from "@astrojs/react";
 export default defineConfig({
   site: import.meta.env.PUBLIC_MY_DOMAIN,
   integrations: [react()],
-  cname: {
-    // Your custom domain (e.g., example.com)
-    domain: import.meta.env.PUBLIC_CUSTOM_DOMAIN,
-    // The canonical name (e.g., www.example.com)
-    canonicalName: import.meta.env.PUBLIC_CUSTOM_CANONICAL_DOMAIN,
-  },
 });
